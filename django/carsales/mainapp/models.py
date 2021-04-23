d6ed6129ed04e5d5e62ff26d7bb191f8eed1ebe5
Dmytrenko-Roman <savitarreus@gmail.com>
2021-04-23 11:07:49 +0300

    😄 Models v1 (without order model) 😒

--- django/carsales/mainapp/models.py
+++ django/carsales/mainapp/models.py
@@ -1,3 +1,79 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.models import ContentType
 
-# Create your models here.
+User = get_user_model()
+
+#-----------
+#1 Category
+#2 Product
+#3 FavoriteProduct
+#4 Favorites
+#5 Order
+#-----------
+#6 Customer
+#7 Specification
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=255, verbose_name='Category name')
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+
+    category = models.ForeignKey(Category, verbose_name='Category', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, verbose_name='Product name')
+    slug = models.SlugField(unique=True)
+    image = models.ImageField(image='Image')
+    description = models.TextField(verbose_name='Description', null=True)
+    price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Price')
+
+    def __str__(self):
+        return self.title
+
+
+class FavoriteProduct(models.Model):
+
+    user = models.ForeignKey('Customer', verbose_name='Buyer', on_delete=models.CASCADE)
+    favorites = models.ForeignKey('Favorites', verbose_name='Favorites', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, verbose_name='Product', on_delete=models.CASCADE)
+    qty = models.PositiveIntegerField(default=1)
+    final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='FinalPrice')
+
+    def __str__(self):
+        return f"Product: {self.product.title} (for favorites)"
+
+
+class Favorites(models.Model):
+
+    owner = models.ForeignKey('Customer', verbose_name='Owner', on_delete=models.CASCADE)
+    products = models.ManyToManyField(FavoriteProduct, blank=True)
+    total_products = models.PositiveIntegerField(default=0)
+    final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='FinalPrice')
+
+    def __str__(self):
+        return str(self.id)
+
+
+class Customer(models.Model):
+
+    user = models.ForeignKey(User, verbose_name='User', on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=20, verbose_name='Phone Number')
+    address = models.CharField(max_length=255, verbose_name='Address')
+
+    def __str__(self):
+        return f'User {self.user.first_name} {self.user.last_name}'
+
+
+class Specification(models.Model):
+
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    name = models.CharField(max_length=255, verbose_name='Product name for specification')
+
+    def __str__(self):
+        return f'Specifications for products: {self.name}'
