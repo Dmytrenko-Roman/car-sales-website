48e848bd3d1d7f46c535c7de14405fd553ff4e04
Dmytrenko-Roman <savitarreus@gmail.com>
2021-04-25 10:59:30 +0300

    🔧 Fixed Category defect 🔧

--- django/carsales/mainapp/admin.py
+++ django/carsales/mainapp/admin.py
@@ -1,10 +1,25 @@
 from django.contrib import admin
+from django import forms
 
-from models import *
+from .models import *
+
+
+class CarCategoryChoiceField(forms.ModelChoiceField):
+    pass
+
+
+class CarAdmin(admin.ModelAdmin):
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return CarCategoryChoiceField(Category.objects.filter(slug='cars'))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
 
 admin.site.register(Category)
-admin.site.register(Car)
+admin.site.register(Car, CarAdmin)
 admin.site.register(Detail)
 admin.site.register(FavoriteProduct)
 admin.site.register(Favorites)
 admin.site.register(Customer)
+
