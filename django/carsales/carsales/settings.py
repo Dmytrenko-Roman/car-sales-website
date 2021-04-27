bd2450a4ad80e42d2bd5893ee07a414e079c9f4d
Dmytrenko-Roman <savitarreus@gmail.com>
2021-04-27 11:19:45 +0300

    🔧 Fixed media root variable 🔧

--- django/carsales/carsales/settings.py
+++ django/carsales/carsales/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static_dev'
