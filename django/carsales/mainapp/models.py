3530385b8a5aa5712b99dadafdbe3620ce7f31d0
Dmytrenko-Roman <savitarreus@gmail.com>
2021-05-05 17:14:03 +0300

    ❌ Deleted some imports ❌

--- django/carsales/mainapp/models.py
+++ django/carsales/mainapp/models.py
@@ -2,13 +2,7 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
-from django.core.files.uploadedfile import InMemoryUploadedFile
 
-from PIL import Image
-
-import sys
-
-from io import BytesIO
 
 User = get_user_model()
 
