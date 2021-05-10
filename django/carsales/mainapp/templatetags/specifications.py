cd46980b5c1d1b4f4f7029e99f2839cc14ae9606
Dmytrenko-Roman <savitarreus@gmail.com>
2021-05-10 12:02:13 +0300

    ✅ Add some content ✅

--- django/carsales/mainapp/templatetags/specifications.py
+++ django/carsales/mainapp/templatetags/specifications.py
@@ -2,6 +2,41 @@ from django import template
 
 register = template.Library()
 
+
+TABLE_HEAD = """
+                <table class="table">
+                  <tbody>
+             """
+
+TABLE_TAIL = """
+                  </tbody>
+                </table>
+             """
+
+TABLE_CONTENT = """
+                    <tr>
+                      <td>{name}</td>
+                      <td>{value}</td>
+                    </tr>
+                """
+
+PRODUCT_SPEC = {
+    'car': {
+        'Model': 'model',
+        'Brand': 'brand',
+        'Year': 'year',
+        'Engine Volume': 'engine_volume',
+        'Country of purchase': 'country_of_purchase',
+        'Number of owners': 'number_of_owners',
+        'Is American': 'is_american',
+    },
+    'detail': {
+        'Brand': 'brand',
+        'Suitable models': 'suitable_models',
+        'Wear': 'wear'
+    }
+}
+
 @register.filter
 def product_spec(product):
     print(product)
