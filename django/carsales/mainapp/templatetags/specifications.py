6f5b733b1ae4be2af531fbe437f90e820dd1d33b
Dmytrenko-Roman <savitarreus@gmail.com>
2021-05-10 12:15:54 +0300

    📏 Added a flexible code 📏

--- django/carsales/mainapp/templatetags/specifications.py
+++ django/carsales/mainapp/templatetags/specifications.py
@@ -1,4 +1,5 @@
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -47,4 +48,4 @@ def get_product_spec(product, model_name):
 @register.filter
 def product_spec(product):
     model_name = product.__class__._meta.model_name
-    return TABLE_HEAD + get_product_spec(product, model_name) + TABLE_TAIL
+    return mark_safe(TABLE_HEAD + get_product_spec(product, model_name) + TABLE_TAIL)
