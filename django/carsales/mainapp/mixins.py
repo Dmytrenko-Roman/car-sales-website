a729bd752910555032f4130d1263dbef5cae2958
Dmytrenko-Roman <savitarreus@gmail.com>
2021-05-16 11:56:39 +0300

    ❄️ Added new functions to CategoryDetailMixin ❄️

--- django/carsales/mainapp/mixins.py
+++ django/carsales/mainapp/mixins.py
@@ -3,4 +3,8 @@ from django.views.generic.detail import SingleObjectMixin
 from .models import Category
 
 class CategoryDetailMixin(SingleObjectMixin):
-    pass
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.get_categories_for_bar()
+        return
