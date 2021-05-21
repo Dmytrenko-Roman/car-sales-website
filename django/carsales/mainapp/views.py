c320dce39d56b660311a9b405b7b8bc6a0d6e137
Dmytrenko-Roman <savitarreus@gmail.com>
2021-05-21 16:18:45 +0300

    ❄️ Get context data ❄️

--- django/carsales/mainapp/views.py
+++ django/carsales/mainapp/views.py
@@ -34,6 +34,11 @@ class ProductDetailView(CategoryDetailMixin, DetailView):
     template_name = 'product_detail.html'
     slug_url_kwarg = 'slug'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['ct_model'] = self.model._meta.model_name
+        return context
+
 
 class CategoryDetailView(CategoryDetailMixin, DetailView):
 
